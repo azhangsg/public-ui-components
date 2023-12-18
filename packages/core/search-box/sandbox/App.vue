88469bcebe95146d648214124f526ea39a7493e2
Val Gorodnichev <val.gorodnichev@konghq.com>
2023-12-18 08:12:16 -0700

    feat: fetch search results

--- packages/core/search-box/sandbox/App.vue
+++ packages/core/search-box/sandbox/App.vue
@@ -1,22 +1,32 @@
 lay<template>
   <div class="sandbox-container">
     <main>
-      <p>This is the component sandbox.</p>
+      <p />
       <SearchBox
         :fetch-field-names="fieldNamesFetcher"
         :fetch-recent-searches="recentSearchesFetcher"
+        :fetch-results="resultsFetcher"
       />
     </main>
   </div>
+  PAT:
+  <p />
+  <input
+    v-model="patKey"
+    type="password"
+  >
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import { SearchBox } from '../src'
+import axios from 'axios'
 
+const patKey = ref('')
 const recentSearchesFetcher = async () => {
   return [
     'A',
-    'AA A',
+    '-AA A',
     '(geo.country: "United Kingdom" OR geo.country: Iceland) AND geo.city: London',
     '@geo.country:("United Kingdom" OR Iceland) @geo.city:LOndon - this pone doesn\'t work till TPS-1813 is resolved',
     'KAuth',
@@ -40,5 +50,21 @@ const fieldNamesFetcher = async () => {
     'labels',
     'type',
   ]
+}
+
+const resultsFetcher = async (q) => {
+  if (!patKey.value) {
+    return { error: 'no pat key' }
+  }
+  const resp = await axios.get('/kong-api/v0/search', {
+    params: {
+      q,
+    },
+    headers: {
+      authorization: `Bearer ${patKey.value}`,
+    },
+  })
+  console.log(resp)
+  return resp.data.data
 }
 </script>
